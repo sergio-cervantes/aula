f9d6171b07a728cbbbc75154f4525fa47f38d4c7
Sergio Cervantes <xerxio@outlook.com>
2025-05-08 11:27:57 -0600

    Permitir acceso externo a "frontend"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
+    allowedHosts: ["frontend", "localhost"],
     watch: {
       usePolling: true,
     },
